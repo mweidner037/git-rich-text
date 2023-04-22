a18debfb36dc303f66519aa24094366b32390ef4
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2023-04-21 21:26:38 -0400

    use write-file-atomic

--- src/main/files.ts
+++ src/main/files.ts
@@ -1,6 +1,7 @@
-import { mkdir, readdir, readFile, writeFile } from "fs/promises";
+import { mkdir, readdir, readFile } from "fs/promises";
 import os from "os";
 import path from "path";
+import writeFileAtomic from "write-file-atomic";
 
 const root = path.join(os.homedir(), "Dropbox/Files/filestore-rich-text-demo");
 
@@ -30,7 +31,8 @@ export async function readAll(): Promise<string[]> {
 export async function write(file: string, data: string): Promise<void> {
   const thePath = path.join(root, file);
   await mkdir(path.dirname(thePath), { recursive: true });
-  await writeFile(thePath, data, { encoding: "utf8" });
+  // Use an atomic write to avoid losing progress if we crash while writing.
+  await writeFileAtomic(thePath, data, { encoding: "utf8" });
 }
 
 // TODO: notify renderer if a file changes (besides our own).
